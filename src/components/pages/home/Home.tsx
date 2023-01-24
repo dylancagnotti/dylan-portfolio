5e01e650d97d67a47f60385ea981233535d76872
Dylan Cagnotti <106975297+dylancagnotti@users.noreply.github.com>
2023-01-24 17:07:48 +0100

    chore: stylings

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
 export const Home = () => {
-  return (
-    <div>
-      <h1 data-interactable data-text-popup="I'm cool yoo">
-        Home
-      </h1>
-    </div>
-  );
+  return <div></div>;
 };
