35e57eddb12adf2b3be85224aea76bc1cd8b7764
Dylan Cagnotti <106975297+dylancagnotti@users.noreply.github.com>
2023-02-01 18:08:06 +0100

    feat: typewriter component

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import '../../../utils/styles/flex.scss';
+import HomeStyle from './Home.module.scss';
+import PresentationWriter from '../../../components/homeTypewriter/HomeTyperwriter';
 
 export const Home = () => {
-  return <div></div>;
+  return (
+    <div className={HomeStyle.homeWrapper}>
+      <div className={HomeStyle.homeGrid}>
+        <div className={HomeStyle.homeGridElement} id={HomeStyle.presentation}>
+          <div data-interactable>
+            <PresentationWriter />
+          </div>
+        </div>
+        <div className={HomeStyle.homeGridElement}>
+          <h1>Home</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
